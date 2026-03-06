fe289e5269add4b7e8aeb0004daeabb9f6aeb006
Julio Arnado <20252d074@khipu.edu.pe>
2026-03-06 18:29:53 -0500

    feat: añadir opciones de ejecución en consola

--- crud.py
+++ crud.py
@@ -55,3 +55,42 @@ def delete_product(product_id):
     _save_data(new_data)
 
     return True
+
+# Ejecutar programa en consola
+if __name__ == "__main__":
+    while True:
+        print("\nCRUD de Productos")
+        print("1. Crear producto")
+        print("2. Ver productos")
+        print("3. Actualizar producto")
+        print("4. Eliminar producto")
+        print("5. Salir")
+
+        option = input("Seleccione una opción: ")
+
+        if option == "1":
+            id = int(input("ID: "))
+            nombre = input("Nombre: ")
+            precio = float(input("Precio: "))
+
+            product = {"id": id, "nombre": nombre, "precio": precio}
+            create_product(product)
+            print("Producto creado.")
+
+        elif option == "2":
+            products = read_products()
+            print(products)
+
+        elif option == "3":
+            id = int(input("ID del producto: "))
+            precio = float(input("Nuevo precio: "))
+            update_product(id, {"precio": precio})
+            print("Producto actualizado.")
+
+        elif option == "4":
+            id = int(input("ID del producto: "))
+            delete_product(id)
+            print("Producto eliminado.")
+
+        elif option == "5":
+            break
